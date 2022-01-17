a60f90e18589e059b782cdc0b962073ed98ed7e5
MansoorAhmadi <82879983+MansoorAhmadi@users.noreply.github.com>
2022-01-17 15:07:23 +0100

    PATIENT

---
+++ Patient.java
@@ -0,0 +1,64 @@
+
+    private long pat_num_HD;
+    private String pat_lastname;
+    private String pat_firstname;
+    private String pat_address;
+    private long pat_tel;
+    private long pat_insurance;
+    private String pat_sub_date;
+
+    public long getPat_num_HD() {
+        return pat_num_HD;
+    }
+
+    public void setPat_num_HD(long pat_num_HD) {
+        this.pat_num_HD = pat_num_HD;
+    }
+
+    public String getPat_lastname() {
+        return pat_lastname;
+    }
+
+    public void setPat_lastname(String pat_lastname) {
+        this.pat_lastname = pat_lastname;
+    }
+
+    public String getPat_firstname() {
+        return pat_firstname;
+    }
+
+    public void setPat_firstname(String pat_firstname) {
+        this.pat_firstname = pat_firstname;
+    }
+
+    public String getPat_address() {
+        return pat_address;
+    }
+
+    public void setPat_address(String pat_address) {
+        this.pat_address = pat_address;
+    }
+
+    public long getPat_tel() {
+        return pat_tel;
+    }
+
+    public void setPat_tel(long pat_tel) {
+        this.pat_tel = pat_tel;
+    }
+
+    public long getPat_insurance() {
+        return pat_insurance;
+    }
+
+    public void setPat_insurance(long pat_insurance) {
+        this.pat_insurance = pat_insurance;
+    }
+
+    public String getPat_sub_date() {
+        return pat_sub_date;
+    }
+
+    public void setPat_sub_date(String pat_sub_date) {
+        this.pat_sub_date = pat_sub_date;
+    }
